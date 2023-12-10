644591d10c4b5a07457c78df698a5e992d8a7fe2
ChristianSpencer7 <147567030+ChristianSpencer7@users.noreply.github.com>
2023-12-10 18:20:36 -0500

    css
    Css for login and signup

--- login.php
+++ login.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
-<html>
-<head>
+<html class="login_php">
     <title>Login Page</title>
+    <link rel="stylesheet" href="signup_login.css">
 </head>
 <body>
 
+<div class = "container1">
+    <h1>LOGIN</h1>
 <form action="validate.php" method="post">
-    Username: <input type="text" name="username"><br>
-    Password: <input type="password" name="password"><br>
-    Role:
-    <select name="role">
+<div><input type="text" name="bUsername" placeholder="Username"></div><br>
+<div>    <input type="password" name="bPassword" placeholder="Password"></div><br>
+<div>    Role:
+       <select name="role">
         <option value="BUYER">BUYER</option>
         <option value="SELLER">SELLER</option>
         <option value="ADMIN">ADMIN</option>
-    </select><br>
-    <input type="submit" value="Login">
+    </select></div><br>
+   <div> <input type="submit" value="Login"> </div>
+    
+</form><br>
+<form action = "signup.php">
+<div class = "sign-up">Not registered?<input type="submit" value="Sign Up" href="signup.php"></div>
 </form>
+</div>
 
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
-<html>
+<html class="signup_php">
 <head>
     <title>Insert Data into Tables</title>
+    <link rel="stylesheet" href="signup_login.css">
 </head>
 <body>
-
+<div class = "container2">
+<h1>SIGN UP</h1>
 <form action="insert_data.php" method="post">
-    First Name: <input type="text" name="bFName"><br>
-    Last Name: <input type="text" name="bLName"><br>
-    Username: <input type="text" name="bUsername"><br>
-    Password: <input type="password" name="bPassword"><br>
-	Role:
+   <div> <input type="text" name="bFName" placeholder="First Name"></div><br>
+   <div> <input type="text" name="bLName" placeholder="Last Name"></div><br>
+   <div> <input type="text" name="bUsername" placeholder="Username"></div><br>
+   <div> <input type="password" name="bPassword" placeholder="Password"></div><br>
+	<div>Role:
     <select name="role">
         <option value="BUYER">BUYER</option>
         <option value="SELLER">SELLER</option>
         <option value="ADMIN">ADMIN</option>
-    </select><br>
-    <input type="submit" value="Submit">
+    </select></div><br>
+    <input type="submit" value="Create Account">
 </form>
-
+</div>
 </body>
 </html>
